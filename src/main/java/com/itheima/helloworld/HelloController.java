2846cf230ff4160cbca7eb9ded7fe0976beac6a6
xy <3196925364@qq.com>
2024-10-26 21:55:39 +0800

    000

--- src/main/java/com/itheima/helloworld/HelloController.java
+++ src/main/java/com/itheima/helloworld/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        System.out.println("Hello World 222...");
-        return "OK! hello world";
+        System.out.println("Hello World 2222...");
+        return "OK! hello world 2222";
     }
 }
